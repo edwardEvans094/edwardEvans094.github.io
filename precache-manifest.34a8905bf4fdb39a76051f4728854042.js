31f87be2f08654ae60fb5adc66e4ea032435d302
Tuan Nguyen <edwardevans094@gmail.com>
2020-09-03 15:20:55 +0700

    Updates

---
+++ precache-manifest.34a8905bf4fdb39a76051f4728854042.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95634cd62c7f84e322227f8e1c552ab",
+    "url": "/stackbow/index.html"
+  },
+  {
+    "revision": "68c20e76b4b2f2a0bfc0",
+    "url": "/stackbow/static/css/2.8392841a.chunk.css"
+  },
+  {
+    "revision": "9d8a64caf6bacdc90686",
+    "url": "/stackbow/static/css/main.6d124736.chunk.css"
+  },
+  {
+    "revision": "68c20e76b4b2f2a0bfc0",
+    "url": "/stackbow/static/js/2.2689805c.chunk.js"
+  },
+  {
+    "revision": "9d8a64caf6bacdc90686",
+    "url": "/stackbow/static/js/main.00eda7bb.chunk.js"
+  },
+  {
+    "revision": "79afe67956fbccf1b3da",
+    "url": "/stackbow/static/js/runtime~main.22128ac6.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/stackbow/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/stackbow/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/stackbow/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/stackbow/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/stackbow/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/stackbow/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/stackbow/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/stackbow/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/stackbow/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/stackbow/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/stackbow/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/stackbow/static/media/sidebar-3.34535b95.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d7b11434b9df190e73ed6e562f4caa6.js"
+  "/stackbow/precache-manifest.34a8905bf4fdb39a76051f4728854042.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stackbow/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
