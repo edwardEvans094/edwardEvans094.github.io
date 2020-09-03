f444662594a365dcd03419ac677cfc871e7db3f3
Tuan Nguyen <edwardevans094@gmail.com>
2020-09-03 14:38:42 +0700

    remove log

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -31,8 +15,8 @@ import AdminLayout from "layouts/Admin.jsx";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Redirect from="*" to="/admin/boost" />
+      <Route path="/" render={props => <AdminLayout {...props} />} />
+      <Redirect from="*" to="/boost" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import NotificationSystem from "react-notification-system";
@@ -74,23 +58,34 @@ class Admin extends Component {
   };
   getRoutes = routes => {
     return routes.map((prop, key) => {
-      console.log("=-===========", prop.layout)
-      if (prop.layout === "/admin") {
-        return (
-          <Route
-            path={prop.layout + prop.path}
-            render={props => (
-              <prop.component
-                {...props}
-                handleClick={this.handleNotificationClick}
-              />
-            )}
-            key={key}
-          />
-        );
-      } else {
-        return null;
-      }
+      return (
+        <Route
+          path={prop.path}
+          render={props => (
+            <prop.component
+              {...props}
+              handleClick={this.handleNotificationClick}
+            />
+          )}
+          key={key}
+        />
+      );
+      // if (prop.layout === "/admin") {
+      //   return (
+      //     <Route
+      //       path={prop.layout + prop.path}
+      //       render={props => (
+      //         <prop.component
+      //           {...props}
+      //           handleClick={this.handleNotificationClick}
+      //         />
+      //       )}
+      //       key={key}
+      //     />
+      //   );
+      // } else {
+      //   return null;
+      // }
     });
   };
   getBrandText = path => {
